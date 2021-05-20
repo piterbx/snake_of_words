491f33da82eed4fdd7eb7c9b14f83a2ff5eecfe0
Piotrek <piotr.bogusz52@wp.pl>
2021-05-20 20:17:54 +0200

    DOM elems prepared, alertService, checkWord & checkInput function added

--- src/app/main-core.js
+++ src/app/main-core.js
@@ -1,1 +1,55 @@
-export const startTest = () => console.log('testing');
+let $mainInput;
+let $mainBtn;
+let $mainAlert;
+let $mainSnake;
+
+export const main = () => {
+    prepareElements();
+    prepareEvents();
+};
+
+const prepareElements = () => {
+    $mainInput = document.querySelector('.input-area__input');
+    $mainBtn = document.querySelector('.input-area__btn');
+    $mainAlert = document.querySelector('.input-area__alert');
+    $mainSnake = document.querySelector('.results__snake');
+};
+
+const prepareEvents = () => {
+    $mainBtn.addEventListener('click', checkInput);
+};
+
+const alertService = evn => {
+    switch(evn){
+        case 1:
+            $mainAlert.innerText = 'Podaj jakieś słowo 😊';
+            break;
+        case 'clear':
+            $mainAlert.innerText = '';
+            break;
+        default:
+            $mainAlert.innerText = 'Nieznany błąd 😿';
+    }
+};
+
+const checkIfRepeat = word => {
+    console.log(word); //temporarily ;)
+};
+
+const checkWord = () => {
+    if(checkIfRepeat($mainInput.value)){
+        console.log('nie znalezioo');
+        $mainInput.value = '';
+    } else {
+        
+    }
+};
+
+const checkInput = () => {
+    if($mainInput.value!=='') {
+        checkWord();
+        alertService('clear');
+    } else {
+        alertService(1);
+    };
+};
